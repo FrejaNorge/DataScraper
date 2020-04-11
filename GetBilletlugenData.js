fde54eeb90107c5c5a1939d3b8d349afd254dca3
creative-break <58289859+creative-break@users.noreply.github.com>
2020-04-11 15:20:36 +0200

    Update
    WebRequest for billetlugen

--- GetBilletlugenData.js
+++ GetBilletlugenData.js
@@ -5,19 +5,4 @@
 
 const request = require('./WebRequest.js');       // Loads the module WebRequest.js
 
-
-let body = request('www.billetlugen.dk','/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&hkId=140&index=0&nurbuchbar=true&show=25&showFilter=yes&sort_by=beginn&sort_direction=asc');
-
-//let header = body.search("<h4><a href=", "class title=");
-
-
-//let mySubString = body.substring(
-//    body.lastIndexOf("<h4><a href=") + 1, 
-//   body.lastIndexOf("class title=")
-//);
-//console.log(mySubString);
-
-let test = body;
-let testPrint = test.split("<h4>");
-
-console.log(testPrint);
+request('www.billetlugen.dk','/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=140&nurbuchbar=true&showFilter=yes&tipps=yes');
