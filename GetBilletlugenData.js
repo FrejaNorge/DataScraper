44879c941e33290210397e9bbe7d05885772a5b6
Nightbue <khpe19@student.aau.dk>
2020-04-21 13:22:42 +0200

    start på eventsortering

--- GetBilletlugenData.js
+++ GetBilletlugenData.js
@@ -42,7 +42,7 @@ for (index = 0; index <= subURL.length; index++) {
             try {
                 result = await request.WebRequestSecure(billetlugenHeader, 'www.billetlugen.dk', subURL[index]);
 
-                console.log(result);
+                //console.log(result);
             } catch(err) {
                 console.log(err);
             }
@@ -77,7 +77,7 @@ for (index = 0; index <= subURL.length; index++) {
             try {
                 result = await request.WebRequestSecure(billetlugenHeader, 'www.billetlugen.dk', accualLink);
 
-                console.log(result);
+                //console.log(result);
             } catch(err) {
                 console.log(err);
             }
@@ -85,4 +85,58 @@ for (index = 0; index <= subURL.length; index++) {
         }, 2000 * (index + counter));
 
     } while ((indexPoint !== -1) && (indexPointDisabled !== -1));
+}
+
+
+let eventInfo = {"link": '', "name": '', "location": '', "place": '', "date": '', "time": '', "price": ''},
+    eventList = [];
+
+let newEventSearch = '<h4>', //let for searchs words
+    eventLinkSearch = '<a href="',
+    linkEndSearch = '" title=',
+    eventNameSearch = 'title="',
+    nameEndSearch = 'name="',
+    eventLocationSearch = 'class="place"',
+    placeStartSearch = '<dt>',
+    placeEndSearch = '</dt>',
+    locationStartSearch = '<span>',
+    locationEndSearch = '</span>',
+    eventDateSearch = '<div>',
+    timeEndSearch = '</div>',
+    eventPriceSearch = 'class="price"',
+    priceStartSearch = 'DKK',
+    priceEndSearch = ',';
+
+let stfu = 'stfu'; //let for lenghts
+
+let existingEventIndex = 0, //let for index places
+    lastEventIndex = 0,
+    linkstartIndex = 0,
+    linkEndIndex = 0,
+    nameStartIndex = 0,
+    nameEndIndex = 0,
+    placeStartIndex = 0,
+    placeEndIndex = 0,
+    locationStartIndex = 0,
+    locationEndIndex = 0,
+    dateStartIndex = 0,
+    dateEndIndex = 0,
+    priceStartIndex = 0,
+    priceEndIndex = 0;
+
+
+existingEventIndex = result.indexOf(newEventSearch, lastEventIndex);
+console.log(existingEventIndex);
+console.log(result);
+
+while (existingEventIndex !== -1) {
+
+    linkstartIndex = result.indexOf(eventLinkSearch, existingEventIndex);
+    linkEndIndex = result.indexOf(linkEndSearch, linkstartIndex);
+
+    eventInfo.link = result.substr((linkstartIndex + eventLinkSearch.lenght), (linkEndIndex - (linkstartIndex - eventLinkSearch.lenght)));
+    console.log(linkstartIndex);
+    console.log(linkEndIndex);
+    console.log(eventInfo.link);
+
 }
