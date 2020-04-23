307ed6916741571c6b49945a6914d5bf60107681
Frederik <34657773+fljzenziot@users.noreply.github.com>
2020-04-23 20:13:49 +0200

    fixed

--- getBilletlugenDataWELP.js
+++ getBilletlugenDataWELP.js
@@ -10,10 +10,9 @@ const billetlugenHeader = require('./headers.js');
 const fs = require('fs');
 
 
-async function getBilletlugenData() {
+function getBilletlugenData() {
 
-    let dataString = "";
-        index = 0,
+    let index = 0,
         subURL = [
         '/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=140&index=0&nextDays=30&nurbuchbar=true&showFilter=yes&sort_by=name&sort_direction=asc',  // Musik
         '/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=141&index=0&nextDays=30&nurbuchbar=true&showFilter=yes&sort_by=name&sort_direction=asc',  // Sport
@@ -21,29 +20,42 @@ async function getBilletlugenData() {
         '/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=164&index=0&nextDays=30&nurbuchbar=true&showFilter=yes&sort_by=name&sort_direction=asc',  // Familie
         '/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=165&index=0&nextDays=30&nurbuchbar=true&showFilter=yes&sort_by=name&sort_direction=asc',  // Comedy
         '/billetter.html?affiliate=DKA&doc=category&fun=kategorieliste&detailadoc=erdetaila&detailbdoc=evdetailb&hkId=166&index=0&nextDays=30&nurbuchbar=true&showFilter=yes&sort_by=name&sort_direction=asc'   // Kultur
-    ];
-
-    for (index = 0; index <= subURL.length; index++) {
-
-        (function(index) {
-
-            setTimeout(async function() {
-                try {
-                    result = await request.WebRequestSecure(billetlugenHeader, 'www.billetlugen.dk', subURL[index]);
-
-                    dataString = result.data;   // Skal gemme data i dataString og returnere strengen som et promise, sådan at jeg kan bruge den async i getDataWELP.js
-
-                } catch(err) {
-                    console.log(err);
+        ];
+    //console.log("1");
+    let p = new Promise(async (resolve, reject) => {
+        let data = [];
+        for (index = 0; index < subURL.length; index++) {
+            try {
+                
+                result = await request.WebRequestSecure(billetlugenHeader, 'www.billetlugen.dk', subURL[index]);
+                console.log(result.statuscode);
+                if(result.statuscode === 200){
+                    data.push(result.data);
                 }
 
-            }, 2000 * index);
-        
-        })(index);
-    };
-    return dataString;
+                if(index + 1 !== subURL.length){//skip last wait
+                    await delay(2000);
+                    //console.log("waited");
+                }
+            } catch(err) {
+                console.log(err);
+                reject(err);
+            }
+            //console.log("2");
+        };
+        //console.log("3");
+        resolve(data);
+    });
+    //console.log("4");
+    return p;
 };
 
+function delay(ms){
+    return new Promise((resolve) =>{
+            setTimeout(resolve, ms);     
+    });
+}
+
 module.exports = { 
-                getBilletlugenData, 
+    getBilletlugenData, 
 }
--- getDataWELP.js
+++ getDataWELP.js
@@ -8,9 +8,9 @@ const request = require('./getBilletlugenDataWELP.js');
 
 const getData = async () => {
 
-    const dataString = await request.getBilletlugenData();
+    let dataString = await request.getBilletlugenData();
     
-    console.log('test',dataString);    // Skal vente på at getBilletlugenData har kørt sådan at jeg kan tilgå en streng med alt HTML data. (Den venter ikke på getBilletlugenData)
+    console.log(dataString);    // Skal vente på at getBilletlugenData har kørt sådan at jeg kan tilgå en streng med alt HTML data. (Den venter ikke på getBilletlugenData)
   
 };
 
